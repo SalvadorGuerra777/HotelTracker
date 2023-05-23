246c00a5680973904a5f3989c949f6aa2379f0d1
Chompi <000004321@uca.edu.sv>
2023-05-22 21:58:57 -0600

    Add_Vistas

--- HotelTracker/app/src/main/java/com/guerra/hoteltracker/MainActivity.kt
+++ HotelTracker/app/src/main/java/com/guerra/hoteltracker/MainActivity.kt
@@ -2,10 +2,21 @@ package com.guerra.hoteltracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.guerra.hoteltracker.databinding.ActivityMainBinding
+import com.guerra.hoteltracker.ui.viewmodel.EmployeeViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
+
 }
